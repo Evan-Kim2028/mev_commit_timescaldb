520e4f82d0e6d1e5fd04ac9dbc5e6e18fb02bcf8
Evan K <ekcopersonal@gmail.com>
2024-11-18 13:05:17 -0500

    fix: add postgREST startup script

---
+++ sql/postgrest/init-postgrest.sql
@@ -0,0 +1,18 @@
+-- Create the api schema if it doesn't exist
+CREATE SCHEMA IF NOT EXISTS api;
+
+-- Create the anon role if it doesn't exist
+DO
+$do$
+BEGIN
+   IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'anon') THEN
+      CREATE ROLE anon NOLOGIN;
+   END IF;
+END
+$do$;
+
+-- Grant usage on the api schema to anon
+GRANT USAGE ON SCHEMA api TO anon;
+
+-- Grant select on all tables in api schema to anon
+ALTER DEFAULT PRIVILEGES IN SCHEMA api GRANT SELECT ON TABLES TO anon;
